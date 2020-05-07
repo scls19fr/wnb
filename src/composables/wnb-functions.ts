b13c0468ed22a4377f807c33e31773eda5e4ffcb
skaar <skaarbjarte@gmail.com>
2020-05-07 21:09:21 +0200

    Minor refacoring

--- src/composables/wnb-functions.ts
+++ src/composables/wnb-functions.ts
@@ -1,8 +1,8 @@
-import { safeLoad } from 'js-yaml';
-import { readFileSync } from 'fs';
+import {safeLoad} from 'js-yaml';
+import {readFileSync} from 'fs';
 import inside from 'point-in-polygon';
-import { AircraftModel, Centrogram, Load, Mass, SliderProperties, Volume } from 'src/models/Aircraft';
-import { GravityCenter } from 'src/models/GravityCenter';
+import {AircraftModel, Centrogram, Load, Mass, SliderProperties, Volume} from 'src/models/Aircraft';
+import {GravityCenter} from 'src/models/GravityCenter';
 
 export class WnbFunctions {
   public static loadConfigYML(filename: string): AircraftModel {
@@ -10,54 +10,49 @@ export class WnbFunctions {
       return safeLoad(readFileSync(filename, 'utf8')) as AircraftModel;
     } catch (e) {
       // tslint:disable-next-line:no-console
-      console.log(e)
+      console.log(e);
     }
     return {} as AircraftModel;
   }
 
   public static defineLoadSliderProperties(sliderProperties: SliderProperties): SliderProperties {
-    sliderProperties.current_value = sliderProperties.default
+    sliderProperties.current_value = sliderProperties.default;
     if (!sliderProperties.step) {
-      sliderProperties.step = 1
+      sliderProperties.step = 1;
     }
     if (sliderProperties.hasOwnProperty('min') && sliderProperties.hasOwnProperty('max')) {
-      sliderProperties.enabled = true
+      sliderProperties.enabled = true;
     } else {
-      sliderProperties.min = sliderProperties.default - sliderProperties.step
-      sliderProperties.max = sliderProperties.default + sliderProperties.step
-      sliderProperties.enabled = false
+      sliderProperties.min = sliderProperties.default - sliderProperties.step;
+      sliderProperties.max = sliderProperties.default + sliderProperties.step;
+      sliderProperties.enabled = false;
     }
-    return sliderProperties
+    return sliderProperties;
   }
 
   public static buildLoadsArray(cfg: AircraftModel): Load[] {
     const loads: Load[] = [];
-    let newLoad
     for (let i = 0; i < cfg.loads.length; i++) {
-      const load = cfg.loads[i]
+      const load = cfg.loads[i];
       if (load.mass) {
-        newLoad = load
-        newLoad.mass = WnbFunctions.defineLoadSliderProperties(newLoad.mass as SliderProperties) as Mass;
-        loads.push(newLoad)
+        load.mass = WnbFunctions.defineLoadSliderProperties(load.mass as SliderProperties) as Mass;
+        loads.push(load);
       } else if (load.volume) {
-        newLoad = load
-        newLoad.volume = WnbFunctions.defineLoadSliderProperties(newLoad.volume as SliderProperties) as Volume;
-        loads.push(newLoad)
-      } /* else { // why push empty object?
-        loads.push({})
-      } */
+        load.volume = WnbFunctions.defineLoadSliderProperties(load.volume as SliderProperties) as Volume;
+        loads.push(load);
+      }
     }
     return loads
   }
 
-  public static calculateGravityCenter(cfg: AircraftModel, loads: Load[]) : GravityCenter {
+  public static calculateGravityCenter(cfg: AircraftModel, loads: Load[]): GravityCenter {
     let mass = 0.0;
-    const gravityCenter : GravityCenter = {
+    const gravityCenter: GravityCenter = {
       mass: 0.0,
       lever_arm: 0.0,
       moment: 0.0
     }
-    for(let i = 0; i < loads.length; i++) {
+    for (let i = 0; i < loads.length; i++) {
       const load = cfg.loads[i]
       if (load.mass) {
         mass = load.mass.current_value;
@@ -73,12 +68,10 @@ export class WnbFunctions {
   }
 
   public static insideCentrogram(G: GravityCenter, centrogram: Centrogram[]) {
-    let polygon: any[];
-    polygon = [];
-    centrogram.forEach((pt: any) => {
+    const polygon: number[][] = [];
+    centrogram.forEach((pt: Centrogram) => {
       polygon.push([pt.lever_arm, pt.mass]);
-    })
-    return inside([G.lever_arm, G.mass], polygon)
+    });
+    return inside([G.lever_arm, G.mass], polygon);
   }
 }
-
