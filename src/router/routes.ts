39f849a9e66e7486de02a79a95ceef8092fcfe11
skaar <skaarbjarte@gmail.com>
2020-05-24 23:16:40 +0200

    Small adds

---
+++ src/router/routes.ts
@@ -0,0 +1,23 @@
+import { RouteConfig } from 'vue-router'
+
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MyLayout.vue'),
+    children: [
+      { path: '', name: 'Index', component: () => import('pages/Index.vue') },
+      { path: 'weight-and-balance', name: 'WeightAndBalance', component: () => import('pages/weight-and-balance/WeightAndBalance.vue') }
+
+    ]
+  }
+]
+
+// Always leave this as last one
+if (process.env.MODE !== 'ssr') {
+  routes.push({
+    path: '*',
+    component: () => import('pages/Error404.vue')
+  })
+}
+
+export default routes
